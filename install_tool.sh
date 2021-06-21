45ae0735ab5d8dc633a7af11579ea86589accf7c
Wei-TianHao <phi.wth@gmail.com>
2021-06-20 21:06:02 -0400

    update

--- install_tool.sh
+++ install_tool.sh
@@ -6,12 +6,19 @@ sudo cp -r julia-1.6.1 /opt/
 sudo ln -s /opt/julia-1.6.0/bin/julia /usr/local/bin/julia
 sudo apt install build-essential -y
 sudo apt install git -y
+sudo apt install python3 -y
+sudo apt install pip -y
+
+
+git clone https://github.com/intelligent-control-lab/NeuralVerification_VNNCOMP_scripts.git
+chmod +x NeuralVerification_VNNCOMP_scripts/*.sh
+pip install -r NeuralVerification_VNNCOMP_scripts/NNet/test_requirements.txt
+
 
 echo '
 using Pkg
 Pkg.add(url="https://github.com/sisl/NeuralVerification.jl")
 Pkg.add("PyCall")
+ENV["PYTHON"]="/usr/bin/python3"
+Pkg.build("PyCall")
 ' | julia
-
-git clone https://github.com/intelligent-control-lab/NeuralVerification_VNNCOMP_scripts.git
-chmod +x NeuralVerification_VNNCOMP_scripts/*.sh
--- run_instance.sh
+++ run_instance.sh
@@ -15,5 +15,5 @@ TIMEOUT=$6
 echo "Running benchmark instance in category '$CATEGORY' with onnx file '$ONNX_FILE', vnnlib file '$VNNLIB_FILE', results file $RESULTS_FILE, and timeout $TIMEOUT"
 
 # run the tool to produce the results file
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-julia "${SCRIPT_DIR}verify_instance.jl" "$ONNX_FILE" "$VNNLIB_FILE" "$RESULTS_FILE"
+script_full_path=$(dirname "$0")
+julia "${script_full_path}/verify_instance.jl" "$ONNX_FILE" "$VNNLIB_FILE" "$RESULTS_FILE"
