86d75047856a49bfb6fcb45ff8fa82497809351c
Wei-TianHao <phi.wth@gmail.com>
2021-06-20 20:37:25 -0400

    add NNet

--- prepare_instance.sh
+++ prepare_instance.sh
@@ -13,7 +13,7 @@ VNNLIB_FILE=$4
 
 echo "Preparing $TOOL_NAME for benchmark instance in category '$CATEGORY' with onnx file '$ONNX_FILE' and vnnlib file '$VNNLIB_FILE'"
 
-if [$CATEGORY != "nn4sys"]
+if [ $CATEGORY != "nn4sys" ]; then
 	echo "Only support nn4sys"
 	exit 1
 fi
@@ -24,6 +24,7 @@ killall -q julia
 # script returns a 0 exit code if successful. If you want to skip a benchmark category you can return non-zero.
 yes n | gzip -kd "$ONNX_FILE"
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-julia "${SCRIPT_DIR}prepare_instance.jl"  "$ONNX_FILE"
+script_name=$0
+script_full_path=$(dirname "$0")
+julia "${script_full_path}/prepare_instance.jl"  "$ONNX_FILE"
 exit 0
